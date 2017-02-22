74f9c29f68653482b4ae3baad0541cd90d9bc48a
Pilipenko Dima <dimpiax@gmail.com>
2017-02-22 23:05:17 +0200

    Update index.js
    A little refactoring and semantic, related to `const, let` and right scope of values.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,42 @@
+const response = (xhr) => {
+	const text = xhr.getAllResponseHeaders(),
+		keys = [],
+		all = [],
+		headers = {},
+		reg = /^\s*(.*?)\s*\:\s*([\s\S]*?)\s*$/gm;
+
+	let res;
+	while (res = reg.exec(text)) {
+		const k = res[1].toLowerCase(), h = headers[k], v = res[2];
+		keys.push(k);
+		all.push([k, v]);
+		headers[k] = (h ? `${h},` : '') + v;
+	}
+
+	return {
+		type: 'cors',
+		ok: xhr.status / 200 | 0 == 1, // 200-399
+		status: xhr.status,
+		statusText: xhr.statusText,
+		url: xhr.responseURL,
+		clone: () => response(xhr),
+		text: () => Promise.resolve(xhr.responseText),
+		json: () => Promise.resolve(xhr.responseText).then(JSON.parse),
+		xml: () => Promise.resolve(xhr.responseXML),
+		blob: () => Promise.resolve(xhr.response),
+		headers: {
+			keys: () => keys,
+			entries: () => all,
+			get: n => headers[n.toLowerCase()],
+			has: n => n.toLowerCase() in headers
+		}
+	};
+};
+
 export default function fetch(url, options) {
 	options = options || {};
-	return new Promise( (resolve, reject) => {
-		let request = new XMLHttpRequest();
-
+	return new Promise((resolve, reject) => {
+		const request = new XMLHttpRequest();
 		request.open(options.method || 'get', url);
 
 		for (let i in options.headers) {
@@ -11,46 +45,11 @@ export default function fetch(url, options) {
 
 		request.onload = () => {
 			resolve(response(request));
-		};
-
+		}
 		request.onerror = () => {
 			reject(Error('Network Error'));
-		};
+		}
 
 		request.send(options.body || null);
-
-		function response(xhr) {
-			let headerText = xhr.getAllResponseHeaders(),
-				keys = [],
-				all = [],
-				headers = {},
-				reg = /^\s*(.*?)\s*\:\s*([\s\S]*?)\s*$/gm,
-				match, key;
-			while ((match=reg.exec(headerText))) {
-				keys.push(key = match[1].toLowerCase());
-				all.push([key, match[2]]);
-				const header = headers[key]
-				headers[key] = (header?(header+','):'') + match[2];
-			}
-
-			return {
-				type: 'cors',
-				ok: xhr.status/200|0 == 1,		// 200-399
-				status: xhr.status,
-				statusText: xhr.statusText,
-				url: xhr.responseURL,
-				clone: () => response(xhr),
-				text: () => Promise.resolve(xhr.responseText),
-				json: () => Promise.resolve(xhr.responseText).then(JSON.parse),
-				xml: () => Promise.resolve(xhr.responseXML),
-				blob: () => Promise.resolve(xhr.response),
-				headers: {
-					keys: () => keys,
-					entries: () => all,
-					get: n => headers[n.toLowerCase()],
-					has: n => !!headers[n.toLowerCase()]
-				}
-			};
-		}
 	});
 }
