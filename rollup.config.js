d1bb3398b340dd514a6a7628b10829f43ee83e41
Jason Miller <jason@developit.ca>
2017-02-23 20:57:11 -0500

    Am I insane? Yes.

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,13 @@
 import buble from 'rollup-plugin-buble';
+import replace from 'rollup-plugin-post-replace';
 
 export default {
 	useStrict: false,
 	plugins: [
-		buble()
+		buble(),
+		replace({
+			'module.exports = index;': '',
+			'var index =': 'module.exports ='
+		})
 	]
 };
