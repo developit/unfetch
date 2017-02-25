37ddcbf8087c5c3e95b4977a5f1dc3303a7f315b
Jason Miller <jason@developit.ca>
2017-02-25 15:08:17 -0500

    Add `dist/unfetch.es.js` output and point modules/jsnext:main at it. Fixes #25.

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,21 @@
 import buble from 'rollup-plugin-buble';
 import replace from 'rollup-plugin-post-replace';
 
+let { FORMAT } = process.env;
+
 export default {
 	useStrict: false,
+	sourceMap: true,
+	entry: 'src/index.js',
 	plugins: [
 		buble(),
-		replace(
-			process.env.FORMAT==='cjs' ? {
-				'module.exports = index;': '',
-				'var index =': 'module.exports ='
-			} : {
-				'return index;': '',
-				'var index =': 'return'
-			}
-		)
+		FORMAT==='cjs' && replace({
+			'module.exports = index;': '',
+			'var index =': 'module.exports ='
+		}),
+		FORMAT==='umd' && replace({
+			'return index;': '',
+			'var index =': 'return'
+		})
 	]
 };
